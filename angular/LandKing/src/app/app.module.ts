edc4ba87904f05279e0815d1936053c75383b288
Fidel <fidelramirez50@yahoo.com>
2022-07-30 13:39:31 -0400

    Added routing to home and sign up

--- angular/LandKing/src/app/app.module.ts
+++ angular/LandKing/src/app/app.module.ts
@@ -2,23 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { RegistrationComponent } from './registration/registration.component';
 import { BannerComponent } from './banner/banner.component';
 import { HomecontentComponent } from './home/homecontent/homecontent.component';
 import { InfobannerComponent } from './infobanner/infobanner.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module'; // Added here
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    RegistrationComponent,
+    routingComponents,
     BannerComponent,
     HomecontentComponent,
     InfobannerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
